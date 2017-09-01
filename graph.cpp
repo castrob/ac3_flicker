5111d602125e5ce407d62bc7052c4e1d48cb235e
igormseixas <igormseixas@hotmail.com>
2017-09-01 18:29:38 -0300

    TODO.txt atualization

---
+++ graph.cpp
@@ -0,0 +1,26 @@
+
+/** Arquitetura de Computadores III - Lesandro Ponciano
+  * Trabalho Pratico 1 - Flicker Database
+  * Integrantes: Igor Machado Seixas
+  *              Joao Castro
+  * Linguagem utilizada: C++
+  * Recursos: <string> substr (int inicio, int quantidade) - Substring
+  *           <string> find (String padrao) - "contains" retorna posicao do padrao se achar, se nao achar returna -1
+  *           <string> at (int posicao) - "charAt" retorna o char da posicao passada por parametro
+  *           <string> stod (string str) - "parseDouble" retorna o valor double da string em parametro
+  */
+  
+  // Imports
+  #include <iostream>
+  //#include <string> // Not Used
+  using namespace std;
+
+
+int main ()
+{
+  //////////////////////// TESTING AREA \\\\\\\\\\\\\\\\\\\\\\
+
+  return 0;
+}
+
+// github.com/castrob/ac3_filcker/TODO.txt to task list
