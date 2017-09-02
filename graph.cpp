abf5915c054d8a29c751b834dc1ef2f306f8aa4e
igormseixas <igormseixas@hotmail.com>
2017-09-02 00:01:54 -0300

    Beginning of implementation of Graph class

--- graph.cpp
+++ graph.cpp
@@ -12,13 +12,111 @@
   
   // Imports
   #include <iostream>
+  #include <list>
+  #include <algorithm>
   //#include <string> // Not Used
   using namespace std;
-
+  
+  #define ARRAY_SIZE 200
+  
+  class graph {
+   public: 
+   // Atributes
+   list<double>* usergraph; // Vertex wich will cotain user id
+   list<double>::iterator it;
+   int n;
+   
+   /**
+     * Graph Construtctor
+     * @param double V - Number of Vertex of the graph
+     */
+     graph ( ) {
+      usergraph = new list<double> [ARRAY_SIZE];      
+      n = 0;
+     }
+     
+   /**
+     * Add Edge to Graph
+     * @param u1 - Vertex User
+     * @param u1 - User to be added
+     */
+     void addEdge ( double u1, double u2 ) {
+       int pos;
+       // Find position of first user
+          pos = binaryFind(u1);
+          usergraph[pos].push_back(u2);
+       // Find position of last user
+          pos = binaryFind(u2);
+          usergraph[pos].push_back(u1);         
+          //it = find(usergraph[0].begin( ), usergraph[0].end( ), 2 );
+     }
+     
+   /**
+     * Add Vertex to Graph
+     * @param v - Vertex to be added
+     */
+     void addVertex (double v) {
+       usergraph[n].push_front(v);
+       n++;
+     }
+     
+   
+   /**
+     * Binary Find
+     * @param u - User wich will be search
+     */
+     int binaryFind ( double u ) {
+       int left = 0, right = n -1, middle;
+       int ans = -1;
+       
+       while (left <= right) {
+         middle = (left+right) /2;
+         
+         if ( u == *usergraph[middle].begin( ) ) {
+           ans = middle;
+           left = ARRAY_SIZE;
+         } else {
+           if ( u > *usergraph[middle].begin( ) ) {
+             left = middle+1;
+           } else { 
+             right = middle-1;
+           } 
+         }
+       } // end while
+       
+       return ans;
+     }
+     
+   /**
+     * Print Graph
+     */
+     void printGraph ( ) {
+       for ( int i = 0; i < n; i++) {
+         for ( list<double>::iterator j = usergraph[i].begin( ); j != usergraph[i].end( ); j++ ) {
+           cout << *j << ' ';
+         }
+       
+         cout << '\n';
+       } // end for
+     }
+     
+  }; // end class graph
 
 int main ()
 {
   //////////////////////// TESTING AREA \\\\\\\\\\\\\\\\\\\\\\
+    
+  graph g;
+    if (g.binaryFind(1) == -1) {
+    g.addVertex(1);
+  }
+    if (g.binaryFind(2) == -1) {
+    g.addVertex(2);
+  }
+  
+  g.addEdge(1,2);
+
+  g.printGraph( );
 
   return 0;
 }
