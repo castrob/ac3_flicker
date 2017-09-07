6f9bea1f5bde447974fe0cf0e497ad2be84854b8
paulojunio <pjrrodrigues@sga.pucminas.br>
2017-09-07 19:44:32 -0300

    Add largeSearch

--- graph.cpp
+++ graph.cpp
@@ -9,32 +9,32 @@
   *           <string> at (int posicao) - "charAt" retorna o char da posicao passada por parametro
   *           <string> stod (string str) - "parseDouble" retorna o valor double da string em parametro
   */
-  
+
   // Imports
   #include <iostream>
   #include <list>
   #include <algorithm>
   //#include <string> // Not Used
   using namespace std;
-  
+
   #define ARRAY_SIZE 200  // Define the length of the study group
-  
+
   class graph {
-   public: 
+   public:
    // Atributes
    list<double>* usergraph; // Vertex wich will cotain user id
    list<double>::iterator it;
    int n;
-   
+
    /**
      * Graph Construtctor
      * @param double V - Number of Vertex of the graph
      */
      graph ( ) {
-      usergraph = new list<double> [ARRAY_SIZE];      
+      usergraph = new list<double> [ARRAY_SIZE];
       n = 0;
      }
-     
+
    /**
      * Add Edge to Graph
      * @param u1 - Vertex User
@@ -42,26 +42,26 @@
      */
      void addEdge ( double u1, double u2 ) {
        int pos1, pos2;
-       
+
        // Verificatins if there is User u1
        pos1 = this->binaryFind(u1);
        if ( pos1 == -1 ) {   // Find and check position of first user
          pos1 = n;  // Update pos1 situation
          this->addVertex(u1);
-       }       
+       }
        // Verificatins if there is User u2
        pos2 = this->binaryFind(u2);
        if ( pos2 == -1 ) { // Find and check position of second user
          pos2 = n; // Update pos2 situation
          this->addVertex(u2);
-       }     
+       }
        // Create Edge between User u1 and User u2
        usergraph[pos1].push_back(u2);
-       usergraph[pos2].push_back(u1); 
-       
+       usergraph[pos2].push_back(u1);
+
        //it = find(usergraph[0].begin( ), usergraph[0].end( ), 2 );
      }
-     
+
    /**
      * Add Vertex to Graph
      * @param v - Vertex to be added
@@ -70,8 +70,8 @@
        usergraph[n].push_front(v);
        n++;
      }
-     
-   
+
+
    /**
      * Binary Find
      * @param u - User wich will be search
@@ -79,25 +79,25 @@
      int binaryFind ( double u ) {
        int left = 0, right = n -1, middle;
        int ans = -1;
-       
+
        while (left <= right) {
          middle = (left+right) /2;
-         
+
          if ( u == *usergraph[middle].begin( ) ) {
            ans = middle;
            left = ARRAY_SIZE;
          } else {
            if ( u > *usergraph[middle].begin( ) ) {
              left = middle+1;
-           } else { 
+           } else {
              right = middle-1;
-           } 
+           }
          }
        } // end while
-       
+
        return ans;
      }
-     
+
    /**
      * Print Graph
      */
@@ -106,19 +106,19 @@
          for ( list<double>::iterator j = usergraph[i].begin( ); j != usergraph[i].end( ); j++ ) {
            cout << *j << ' ';
          }
-       
+
          cout << '\n';
        } // end for
      }
-     
+
   }; // end class graph
 
 int main ()
 {
   //////////////////////// TESTING AREA \\\\\\\\\\\\\\\\\\\\\\\
-    
+
   graph g;
-  
+
   g.addEdge(1,2);
   /*g.addEdge(1,123.312);
   g.addEdge(1,4);
--- graph.h
+++ graph.h
@@ -10,32 +10,43 @@
   *           <string> at (int posicao) - "charAt" retorna o char da posicao passada por parametro
   *           <string> stod (string str) - "parseDouble" retorna o valor double da string em parametro
   */
-  
+
   // Imports
   #include <iostream>
   #include <list>
   #include <algorithm>
   //#include <string> // Not Used
   using namespace std;
-  
+
   #define ARRAY_SIZE 200  // Define the length of the study group
-  
+
   class graph {
-   public: 
+   public:
    // Atributes
+   int nivelZero;
+   int nivelPrimeiro;
+   int nivelSegundo;
+   int nivelTerceiro;
+   int nivelQuarto;
+
    list<double>* usergraph; // Vertex which will cotain user id
    list<double>::iterator it;
    int n;
-   
+
    /**
      * Graph Construtctor
      * @param double V - Number of Vertex of the graph
      */
      graph ( ) {
-      usergraph = new list<double> [ARRAY_SIZE];      
+      usergraph = new list<double> [ARRAY_SIZE];
       n = 0;
+      nivelZero = 0;
+      nivelQuarto = 0;
+      nivelSegundo = 0;
+      nivelTerceiro = 0;
+      nivelPrimeiro = 0;
      }
-     
+
    /**
      * Add Edge to Graph
      * @param u1 - Vertex User
@@ -43,40 +54,40 @@
      */
      void addEdge ( double u1, double u2 ) {
        int pos1, pos2;
-       
+
        // Verificating if there is User u1
-       pos1 = SequencialSearch(u1);
+       pos1 = sequencialSearch(u1);
        if ( pos1 == -1 ) {   // Find and check position of first user
          pos1 = n;  // Update pos1 situation
          addVertex(u1);
-       }       
+       }
        // Verificating if there is User u2
-       pos2 = SequencialSearch(u2);
+       pos2 = sequencialSearch(u2);
        if ( pos2 == -1 ) { // Find and check position of second user
          pos2 = n; // Update pos2 situation
          addVertex(u2);
-       }     
+       }
        // Create Edge between User u1 and User u2
        usergraph[pos1].push_back(u2);
-       usergraph[pos2].push_back(u1); 
-       
+       usergraph[pos2].push_back(u1);
+
        //it = find(usergraph[0].begin( ), usergraph[0].end( ), 2 );
      }
-     
+
    /**
      * Add Vertex to Graph
      * @param v - Vertex to be added
      */
      void addVertex (double v) {
       int pos;
-      pos = SequencialSearch(v);
+      pos = sequencialSearch(v);
       if (pos == -1){
        usergraph[n].push_front(v);
        n++;
       }
      }
-     
-   
+
+
    /**
      * Binary Find
      * @param u - User which will be search
@@ -84,22 +95,22 @@
      int binaryFind ( double u ) {
        int left = 0, right = n -1, middle;
        int ans = -1;
-       
+
        while (left <= right) {
          middle = (left+right) /2;
-         
+
          if ( u == *usergraph[middle].begin( ) ) {
            ans = middle;
            left = ARRAY_SIZE;
          } else {
            if ( u > *usergraph[middle].begin( ) ) {
              left = middle+1;
-           } else { 
+           } else {
              right = middle-1;
-           } 
+           }
          }
        } // end while
-       
+
        return ans;
      }
 
@@ -108,7 +119,7 @@
       * @param u - user which will be search
       */
 
-     int SequencialSearch(double u){
+     int sequencialSearch(double u){
       int answer = -1;
       for(int i = 0; i < n; i++){
         if( u == *usergraph[i].begin())
@@ -117,7 +128,7 @@
       return answer;
      }
 
-     
+
    /**
      * Print Graph
      */
@@ -126,19 +137,72 @@
          for ( list<double>::iterator j = usergraph[i].begin( ); j != usergraph[i].end( ); j++ ) {
            cout << *j << ' ';
          }
-       
+
          cout << '\n';
        } // end for
      }
-     
+     void largeSearch(double idOwner , double idFriend) {
+       int n0 = sequencialSearch(idOwner);
+       for ( list<double>::iterator i = usergraph[n0].begin( ); i != usergraph[n0].end( ); i++ ) {
+          if(*i == idFriend) {
+
+            nivelZero++;
+
+          }
+          int n1 = sequencialSearch(*i);
+          for( list<double>::iterator j = usergraph[n1].begin( ); j != usergraph[n1].end( ); j++ ) {
+
+            if(*j == idFriend) {
+
+              nivelPrimeiro++;
+
+            }
+            int n2 = sequencialSearch(*j);
+
+            for( list<double>::iterator k = usergraph[n2].begin( ); k != usergraph[n2].end( ); k++ ) {
+
+              if(*k == idFriend) {
+
+                nivelSegundo++;
+
+              }
+              int n3 = sequencialSearch(*k);
+
+
+
+            for( list<double>::iterator l = usergraph[n3].begin( ); l != usergraph[n3].end( ); l++ ) {
+
+              if(*l == idFriend) {
+
+                nivelTerceiro++;
+
+              }
+              int n4 = sequencialSearch(*l);
+
+
+
+                for( list<double>::iterator m = usergraph[n4].begin( ); m != usergraph[n4].end( ); m++ ) {
+
+                  if(*m == idFriend) {
+
+                    nivelQuarto++;
+
+                  }
+
+                }
+              }
+            }
+          }
+       }
+     }
   }; // end class graph
 
 //   //////////////////////// TESTING AREA \\\\\\\\\\\\\\\\\\\\\\
 // int main ()
 // {
-    
+
 //   graph g;
-  
+
 //   g.addEdge(1,2);
 //   g.addEdge(1,123.312);
 //   g.addEdge(1,4);
